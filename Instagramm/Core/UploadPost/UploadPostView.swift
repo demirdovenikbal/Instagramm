ccef2a9dfc2d3eb7c64fe83b3a6d81ad8cc39500
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2025-08-03 02:03:58 -0700

    Implemented the UploadPost view and viewmodel

---
+++ Instagramm/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,18 @@
+//
+//  UploadPostView.swift
+//  Instagramm
+//
+//  Created by Ikbal Demirdoven on 2025-08-03.
+//
+
+import SwiftUI
+
+struct UploadPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UploadPostView()
+}
---
+++ Instagramm/Core/UploadPost/UploadPostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UploadPostViewModel.swift
+//  Instagramm
+//
+//  Created by Ikbal Demirdoven on 2025-08-03.
+//
+
+import Foundation
