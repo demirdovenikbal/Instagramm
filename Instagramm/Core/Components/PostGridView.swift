d7f4603558fef063a66288a16a87d33f04f47f76
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2025-08-03 00:20:47 -0700

    Made the distinction between CurrentUserProfileView and ProfileView

---
+++ Instagramm/Core/Components/PostGridView.swift
@@ -0,0 +1,18 @@
+//
+//  PostGridView.swift
+//  Instagramm
+//
+//  Created by Ikbal Demirdoven on 2025-08-02.
+//
+
+import SwiftUI
+
+struct PostGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostGridView()
+}
